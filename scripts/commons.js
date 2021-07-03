54466c9c5cd5404f3ff88502a7990bb5d5247728
Or Linzer <orlinzer@gmail.com>
2021-07-03 16:28:56 +0300

    I moved to a new computer :)

--- scripts/commons.js
+++ scripts/commons.js
@@ -3,6 +3,16 @@
 var themeButton;
 var menuButton;
 var searchButton;
+
+/**
+ * @type HTMLInputElement
+ */
+var searchBox;
+
+/**
+ * @type HTMLFormElement
+ */
+var searchForm;
 // var menu;
 
 function toggleTheme (e) {
@@ -13,8 +23,20 @@ function toggleMenu (e) {
   document.documentElement.classList.toggle('open-menu');
 }
 
-function toggleSearch (e) {
-  document.documentElement.classList.toggle('open-search');
+function openSearch (e) {
+  if (!document.documentElement.classList.contains('open-search')) {
+    document.documentElement.classList.add('open-search');
+    searchBox.focus();
+  } else {
+    searchForm.submit();
+  }
+}
+
+function closeSearch (e) {
+  setTimeout(() => {
+    searchBox.value = '';
+    document.documentElement.classList.remove('open-search');
+  }, 200);
 }
 
 function load () {
@@ -22,10 +44,13 @@ function load () {
   themeButton = document.getElementById('theme-button');
   menuButton = document.getElementById('menu-button');
   searchButton = document.getElementById('search-button');
+  searchBox = document.getElementById('search-box');
+  searchForm = document.getElementById('search');
 
   themeButton.onclick = toggleTheme;
   menuButton.onclick = toggleMenu;
-  searchButton.onclick = toggleSearch;
+  searchButton.onclick = openSearch;
+  searchBox.onblur = closeSearch;
 }
 
 window.onload = load;
